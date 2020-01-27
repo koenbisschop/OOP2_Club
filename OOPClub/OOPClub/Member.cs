aa9d94a2b7ad702a184bdb360a95c19cd0e56d2a
koenbisschop <koen.bisschop@hotmail.be>
2020-01-27 12:27:17 +0100

    Oplossing taak Len

---
+++ OOPClub/OOPClub/Member.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPClub
+{
+    public enum AgeCategories
+    {
+        Kind,
+        Adolecent,
+        Jongere,
+        Volwassene,
+        Undefined
+    }
+    class Member
+    {
+        string _name;
+        DateTime _dob;
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            private set
+            {
+                _name = value;
+            }
+        }
+
+        public DateTime Dob
+        {
+            get
+            {
+                return _dob;
+            }
+            private set
+            {
+                _dob = value;
+            }
+        }
+
+        public AgeCategories AgeCategory
+        {
+            get
+            {
+                return GetAgeCategory();
+            }
+        }
+        public Member(string name, DateTime dob)
+        {
+            Name = name;
+            Dob = dob;
+        }
+
+        public AgeCategories GetAgeCategory()
+        {
+            AgeCategories ageCategory = AgeCategories.Undefined;
+
+            if (Age <= 8)
+                ageCategory = AgeCategories.Kind;
+            if (Age <= 15 && Age > 8)
+                ageCategory = AgeCategories.Adolecent;
+            if (Age < 18 && Age > 15)
+                ageCategory = AgeCategories.Jongere;
+            if (Age > 18)
+                ageCategory = AgeCategories.Volwassene;
+
+            return ageCategory;
+        }
+
+        public int Age
+        {
+            get
+            {
+                int age = DateTime.Today.Year - Dob.Year;
+                if (DateTime.Today.Month <= Dob.Month)
+                {
+                    age--;
+                    if (DateTime.Today.Month == Dob.Month && DateTime.Today.Day >= Dob.Day)
+                        age++;
+                }
+                return age;
+            }
+        }
+
+        public decimal Price
+        {
+            get
+            {
+                decimal price = 0M;
+
+                if (AgeCategory == AgeCategories.Kind)
+                    price = 0M;
+                if (AgeCategory == AgeCategories.Adolecent)
+                    price = 10M;
+                if (AgeCategory == AgeCategories.Jongere)
+                    price = 15M;
+                if (AgeCategory == AgeCategories.Volwassene)
+                    price = 21M;
+
+                return price;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}, {Age} jaar oud | {AgeCategory} | {Price.ToString("C2")} te betalen";
+        }
+    }
+}
--- OOPClub/OOPClub/Program.cs
+++ OOPClub/OOPClub/Program.cs
@@ -8,8 +8,71 @@ namespace OOPClub
 {
     class Program
     {
+        public static List<Member> members = new List<Member>();
         static void Main(string[] args)
         {
+
+            bool end = false;
+
+            while (!end)
+            {
+                string name = GetName();
+                if (name.ToUpper() == "STOP")
+                    end = true;
+                else
+                {
+                    DateTime dob = GetDob();
+                    members.Add(new Member(name, dob));
+                }
+                Console.Clear();
+            }
+
+            decimal totalMoney = MonthlyPay(members);
+
+            foreach (Member m in members)
+            {
+                Console.WriteLine(m);
+            }
+            Console.WriteLine($"Maandelijks inkomen: {totalMoney.ToString("C2")}");
+            Console.ReadLine();
+        }
+
+        static string GetName()
+        {
+            Console.WriteLine("Geef uw volledige naam.");
+            return Console.ReadLine();
+        }
+
+        static DateTime GetDob()
+        {
+            DateTime dob = DateTime.Today;
+            bool inputOK = false;
+            while (!inputOK)
+            {
+                Console.WriteLine("Geef uw geboortedatum. (MM/DD/YYYY)"); // MM/DD/YY omdat mijn laptop en PC in het Engels(verenigde Staten) staan
+                try
+                {
+                    dob = Convert.ToDateTime(Console.ReadLine());
+                    inputOK = true;
+                }
+                catch (FormatException fe)
+                {
+                    Console.WriteLine(fe.Message);
+                }
+            }
+            return dob;
+
+        }
+
+        static decimal MonthlyPay(List<Member> members)
+        {
+            decimal money = 0M;
+            foreach (Member m in members)
+            {
+                money += m.Price;
+            }
+            return money;
         }
     }
+
 }
